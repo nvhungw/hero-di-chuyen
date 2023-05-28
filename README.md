00b8a845fa67469a261be4609134c65e119260e2
nvhungw <119866957+nvhungw@users.noreply.github.com>
2023-05-28 21:19:43 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hero-di-chuyen
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-6uq4t7)
---
+++ script.js
@@ -0,0 +1,68 @@
+console.log('Hello!');
+class Hero{
+  constructor(image, top, left, size,speed){
+    this.image = image;
+    this.top = top;
+    this.left = left;
+    this.size = size;
+    this.speed = speed
+  }
+
+  getHeroElement(){
+    return '<img width="'+ this.size + '"' +
+      ' height="'+ this.size + '"' +
+      ' src="' + this.image +'"' +
+      ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
+  }
+
+ moveRight(){
+    this.left += this.speed;
+    console.log('left: ' + this.left);
+  }
+
+  moveBottom(){
+    this.top += this.speed;
+    console.log('left: ' + this.left);
+  }
+
+}
+
+var hero = new Hero('pikachu.png', 20, 30, 200,30);
+
+function start(){
+  let windowWidth = window.innerWidth
+  let windowHeight = window.innerHeight
+
+  let isRunToRight = hero.left < windowWidth - hero.size;
+  let isRunToBottom = hero.top < windowHeight - hero.size;
+
+  console.log(isRunToRight)
+  if(isRunToRight){
+    hero.moveRight();
+    document.getElementById('game').innerHTML = hero.getHeroElement();
+    setTimeout(start, 1000)
+  }
+  else {
+    if(isRunToBottom)
+    {
+      console.log(hero.getHeroElement())
+      hero.moveBottom();
+      document.getElementById('game').innerHTML = hero.getHeroElement();
+      setTimeout(start, 1000)
+    }
+  }
+  // else{
+  //   if(isRunToBottom)
+  // {
+  //   hero.moveBottom();
+  //   document.getElementById('game').innerHTML = hero.getHeroElement();
+  //     setTimeout(start, 1000)
+  // }
+   
+  // }
+ 
+
+ 
+}
+
+start();
